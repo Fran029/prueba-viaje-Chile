5b75728206984e0f0c94b8c0ef20ed979d502af4
Ion <ioncucer@gmail.com>
2024-03-17 13:13:15 -0300

    Arreglo del formulario Bootstrap con el botón JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-<script>
-    
-    function submitForm () {
-        // Obtener los valores del formulario
-        var name = document.getElementById('name').value;
-        var email = document.getElementById('email').value;
-        var message = document.getElementById('message').value;
-            
-        window.location.href = 'confirmation.html';
-        alert ("mensaje enviado")
-    }
-</script>
+function submitForm() {
+    // Obtener los valores del formulario
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Crear un objeto con los datos del formulario
+    var formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('message', message);
+
+    // Aquí normalmente realizarías alguna acción con los datos del formulario, pero como no tienes un servidor, simplemente mostraremos una alerta con los datos para demostración
+    alert("Datos del formulario:\Envio exitoso \nNombre: " + name + "\nCorreo electrónico: " + email + "\nMensaje: " + message);
+}
